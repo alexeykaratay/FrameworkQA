7dbba683521e214a1eafbfd69fe98e72d75bbded
Alexey Karatay <alexeykaratay@yandex.ru>
2023-03-02 02:19:19 +0300

    First commit

---
+++ src/main/java/pages/realt_home/RealtHomePage.java
@@ -0,0 +1,11 @@
+package pages.realt_home;
+
+import org.openqa.selenium.WebDriver;
+import pages.base.BasePage;
+
+public class RealtHomePage extends BasePage {
+
+    public RealtHomePage(WebDriver driver) {
+        super(driver);
+    }
+}
